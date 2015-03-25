2aa76700791aee83a7e7fb516283a6132101b5f8
WINNIE_YUEN <marcatlan@WINNIE-YUEN.local>
2015-03-24 17:48:07 -0700

    Setting up project

---
+++ RenameMiami.java
@@ -0,0 +1,14 @@
+//Credit to crunchify for tutorial
+//http://crunchify.com/how-to-run-windowsmac-commands-in-java-and-return-the-text-result/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+ 
+ public class RenameMiami {
+ 	public static void main(String[] args){
+ 		String s = null;
+
+ 	}
+ }
