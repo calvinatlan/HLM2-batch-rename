85d411f05171941000001b10f37f6b37c89b84df
WINNIE_YUEN <marcatlan@WINNIE-YUEN.local>
2015-03-24 18:57:51 -0700

    Reads in tracklist, creates a fileinputstream of it, and reads all files in music folder

--- RenameMiami.java
+++ RenameMiami.java
@@ -1,14 +1,35 @@
-//Credit to crunchify for tutorial
-//http://crunchify.com/how-to-run-windowsmac-commands-in-java-and-return-the-text-result/
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+//Rename Miami
+//by Calvin Atlan
+//
+//Renames all songs in a folder based on a tracklist
+//
+//Credit to stackoverflow
+//http://stackoverflow.com/questions/1844688/read-all-files-in-a-folder
  
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
  public class RenameMiami {
  	public static void main(String[] args){
- 		String s = null;
+
+ 		//Tracklist file
+ 		File tracklist = new File("/Users/marcatlan/Desktop/Calvin Projects/HLM2-batch-rename/hlm2-tracklist.txt");
+
+ 		//Read in all lines of the tracklist
+ 		FileInputStream fstream = new FileInputStream(tracklist);
+ 		BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+
+ 		//Folder containing songs
+ 		File folder = new File("/Users/marcatlan/Desktop/HLM2/");
+ 		File[] listOfFiles = folder.listFiles();
+
+		for (File file : listOfFiles) {
+   			 if (file.isFile()) {
+   			 	String name = file.getName();
+      	  		//System.out.println(name);
+    		}
+		}
 
  	}
  }
